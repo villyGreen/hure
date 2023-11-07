fe0a5dbbb164aa59aa89d82380ab26007408e1fd
lashlyn <vat.13@list.ru>
2023-11-07 22:18:00 +0700

    Initial Commit

---
+++ Hure_demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hure_demo
+//
+//  Created by vadim.vitkovskiy on 07.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
